04d04bf817ec40f8239c1ef3b529e93340d9ba70
derekchan <chendachao@addcn.com>
2019-12-04 14:01:24 +0800

    t8891 line boost component

--- src/route/routes.php
+++ src/route/routes.php
@@ -4,4 +4,4 @@ Route::group(['namespace' => config('boost.route.namespace'), 'middleware' => co
     Route::get('{compaign}/line/data', 'BoostController@lineData');
     Route::post('{compaign}/line/auth', 'BoostController@lineAuth');
     Route::post('{compaign}/boost', 'BoostController@boost');
-}
+});
