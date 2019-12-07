3925d125c7586ab46ba162b84f16587b20cf09fc
derekchan <chendachao@addcn.com>
2019-12-07 14:11:05 +0800

    update route

--- src/Route/routes.php
+++ src/Route/routes.php
@@ -1,8 +1,8 @@
 <?php
 
 Route::group(['namespace' => config('boost.route.namespace'), 'middleware' => config('boost.route.middleware'), 'prefix' => config('boost.route.prefix')], function () {
-    Route::get('{compaign}/line/data', 'BoostController@lineData');
-    Route::get('{compaign}/sponsor/auth', 'BoostController@sponsorAuth');
-    Route::get('{compaign}/user/auth', 'BoostController@userAuth');
-    Route::get('{compaign}/boost', 'BoostController@boost');
+    Route::any('{compaign}/line/data', 'BoostController@lineData');
+    Route::any('{compaign}/sponsor/auth', 'BoostController@sponsorAuth');
+    Route::any('{compaign}/user/auth', 'BoostController@userAuth');
+    Route::any('{compaign}/boost', 'BoostController@boost');
 });
